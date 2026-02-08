09c3118247470ff9717c2a95ec2e8ce86315076a
aeon <jerry73204@gmail.com>
2026-02-08 21:55:02 +0800

    Complete scripts for sensor checking

--- scripts/check/run.sh
+++ scripts/check/run.sh
@@ -4,4 +4,4 @@ script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd "$script_dir"
 repo_dir="$script_dir/../.."
 source "$repo_dir/install/setup.bash"
-play_launch launch sensors.launch.xml
+ros2 launch sensors.launch.xml
